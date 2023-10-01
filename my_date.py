3d7c426d08ae78057c93ae950ea7aea58346e714
Sathyadeepthi <145634733+Sathyadeepthi@users.noreply.github.com>
2023-10-01 10:58:56 +0530

    Update my_date.py
    project

--- my_date.py
+++ my_date.py
@@ -1,25 +1,22 @@
 def is_leap_year(year: int) -> bool:
-    """Return True if year is a leap year, False otherwise"""
+    """Return True if year is a leap year, False oterwise"""
     return (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0))
 
-
-def ordinal_date(year: int, month: int, day: int) -> int:
+def ordinal_date(year:int , month: int, day: int) -> int:
     """ Return the number of days elapsed since the beginning of the year, including any partial days.
         For example, the ordinal date for 1 January is 1.
         Hint: pre-compute the ordinal date for the first of each month."""
     days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
+    
     if is_leap_year(year):
         days_in_month[2] = 29
-
-    return sum(days_in_month[1:month]) + day
-
+    
+    return sum(days_in_month[1:month]) + day    
 
 def days_in_a_year(year):
     return 366 if is_leap_year(year) else 365
 
-
-def days_elapsed(year1: int, month1: int, day1: int, year2: int, month2: int, day2: int) -> int:
+def days_elapsed(year1: int, month1: int, day1: int, year2: int, month2: int, day2:int ) -> int:
     """ Return the number of days that have elapsed between year1-month1-day1 and year2-month2-day2.
         You may assume that year1-month1-day1 falls on or before year2-month2-day2. (In other words,
         your answer will always be >= 0.) """
@@ -33,9 +30,7 @@ def days_elapsed(year1: int, month1: int, day1: int, year2: int, month2: int, da
 
     return total_days + days_in_a_year(year1) - ordinal_date(year1, month1, day1) + ordinal_date(year2, month2, day2)
 
-
 DAYS_OF_WEEK = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-
 
 def day_of_week(year: int, month: int, day: int) -> str:
     """ Return the day of the week (Sunday, Monday, Tuesday, etc.) for the given day
@@ -47,20 +42,19 @@ def day_of_week(year: int, month: int, day: int) -> str:
 
     percent = year // 100
     year_percent = year % 100
-
+    
     day_cnt = (day + ((13 * (month + 1)) // 5) + year_percent +
-               (year_percent // 4) + (percent // 4) - (2 * percent))
-
+            (year_percent // 4) + (percent // 4) - (2 * percent))
+    
     day_num = day_cnt % 7
-
+    
     return DAYS_OF_WEEK[day_num - 1]
-
 
 def to_str(year: int, month: int, day: int) -> str:
     """ Return this date as string of the form "Wednesday, 07 March 1833"."""
-    mnt_names = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-                 "November", "December"]
+    mnt_names = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     d_name = day_of_week(year, month, day)
     m_name = mnt_names[month]
     return f"{d_name}, {day:02d} {m_name} {year}"
+
 
